ddb6bdaa1aeea76e05a0d818749592647dedccf7
Sakib Noman <skbnmn@gmail.com>
2021-01-30 18:44:39 +0600

    confirmation screen added

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
-document.getElementById("first-class-plus").addEventListener("click", function () {
-    btnPlusMinus("first-class-input",true);
+document.getElementById("first-class-plus").addEventListener("click", function() {
+    btnPlusMinus("first-class-input", true);
 })
-document.getElementById("first-class-minus").addEventListener("click", function () {
-    btnPlusMinus("first-class-input",false);
+document.getElementById("first-class-minus").addEventListener("click", function() {
+    btnPlusMinus("first-class-input", false);
 })
-document.getElementById("economy-class-plus").addEventListener("click", function () {
-    btnPlusMinus("economy-class-input",true);
+document.getElementById("economy-class-plus").addEventListener("click", function() {
+    btnPlusMinus("economy-class-input", true);
 })
-document.getElementById("economy-class-minus").addEventListener("click", function () {
-    btnPlusMinus("economy-class-input",false);
+document.getElementById("economy-class-minus").addEventListener("click", function() {
+    btnPlusMinus("economy-class-input", false);
 })
 
 //function for increment or decrement
-function btnPlusMinus(id,isPlus) {
+function btnPlusMinus(id, isPlus) {
     var numOfTicket = document.getElementById(id).value;
     var numOfTicketInt = parseInt(numOfTicket);
-    if(isPlus == true){
+    if (isPlus == true) {
         numOfTicketInt++;
-    }
-    else{   //prevent minus input
-        if(numOfTicketInt>0){
+    } else { //prevent minus input
+        if (numOfTicketInt > 0) {
             numOfTicketInt--;
         }
     }
@@ -29,42 +27,44 @@ function btnPlusMinus(id,isPlus) {
 }
 
 //function for calculating subtotal and calling vat function
-function subTotal(){
+function subTotal() {
     const firstClassFair = 150;
     const economyClassFair = 100;
     var firstClass = parseInt(document.getElementById("first-class-input").value);
     var economyClass = parseInt(document.getElementById("economy-class-input").value);
-    var subTotalInt = (firstClass*firstClassFair) + (economyClass*economyClassFair);
-    document.getElementById("subtotal").innerText = "$ "+subTotalInt;
+    var subTotalInt = (firstClass * firstClassFair) + (economyClass * economyClassFair);
+    document.getElementById("subtotal").innerText = "$ " + subTotalInt;
     vat(subTotalInt);
 
-    //checking if ticket number is 0 for alert screen
-    if(firstClass == 0 && economyClass == 0){
+    //checking if ticket number is 0 for confirmation screen
+    if (firstClass == 0 && economyClass == 0) {
         return false;
     }
     return true;
 }
 
 //function for calculating vat and calling grandTotal function
-function vat(subTotalInt){
+function vat(subTotalInt) {
     vatRate = 0.10;
-    var totalVat = subTotalInt*vatRate;
-    document.getElementById("vat").innerText = "$ "+totalVat;
-    grandTotal(subTotalInt,totalVat);
+    var totalVat = subTotalInt * vatRate;
+    document.getElementById("vat").innerText = "$ " + totalVat;
+    grandTotal(subTotalInt, totalVat);
 }
 
 //function for calculating grand total
-function grandTotal(subTotalInt,totalVat){
-    document.getElementById("total").innerText = "$ "+(subTotalInt+totalVat);
+function grandTotal(subTotalInt, totalVat) {
+    document.getElementById("total").innerText = "$ " + (subTotalInt + totalVat);
 }
 
-
-
-document.getElementById("book-btn").addEventListener("click",function(){
-    if(subTotal()==false){
-        alert("Please Select at least one ticket")
-    }
-    else{
-        alert("Successfully booked!!!\nThank you")
+//functionality of confirmation part
+document.getElementById("book-btn").addEventListener("click", function() {
+    if (subTotal() == false) {
+        document.getElementById("main-part").style.display = "none";
+        document.getElementById("confirmation-text").innerText = "Your Cart is Empty!!!";
+        document.getElementById("confirmation-text").style.color = "red";
+        document.getElementById("confirmation-page").style.display = "block"
+    } else {
+        document.getElementById("main-part").style.display = "none";
+        document.getElementById("confirmation-page").style.display = "block"
     }
 })
